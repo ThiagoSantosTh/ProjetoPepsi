7ab6f1fed536b1a0c2cafbf94d1ce53414764a41
Thiago Santos <61941921+ThiagoSantosTh@users.noreply.github.com>
2021-07-22 11:00:35 -0300

    Modo Noturno

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(imagemPepsi){
+    document.querySelector('.pepsi').src = imagemPepsi
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec')
+    sec.style.background = color
+}
